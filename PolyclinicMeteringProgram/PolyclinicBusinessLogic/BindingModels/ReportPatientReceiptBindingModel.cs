664edb6a1e0f1046d97b67d5fb941cf7f13c21f8
shovk <shovk@DESKTOP-NSNKSRN>
2021-04-08 21:21:57 +0400

    косячок исправил

--- PolyclinicMeteringProgram/PolyclinicBusinessLogic/BindingModels/ReportPatientReceiptBindingModel.cs
+++ PolyclinicMeteringProgram/PolyclinicBusinessLogic/BindingModels/ReportPatientReceiptBindingModel.cs
@@ -2,7 +2,7 @@
 
 namespace PolyclinicBusinessLogic.BindingModels
 {
-    class ReportPatientReceiptBindingModel
+    public class ReportPatientReceiptBindingModel
     {
         public string FileName { get; set; }
         public int DoctorId { get; set; }
