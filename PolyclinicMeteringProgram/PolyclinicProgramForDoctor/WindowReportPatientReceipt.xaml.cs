50a0bc5a3ba244c47f4f395ab9a5e3186b174407
shovk <shovk@DESKTOP-NSNKSRN>
2021-05-19 16:19:29 +0400

    repair

--- PolyclinicMeteringProgram/PolyclinicProgramForDoctor/WindowReportPatientReceipt.xaml.cs
+++ PolyclinicMeteringProgram/PolyclinicProgramForDoctor/WindowReportPatientReceipt.xaml.cs
@@ -24,7 +24,7 @@ namespace PolyclinicMeteringProgram
         }
         private void ReportViewer_Load(object sender, EventArgs e)
         {
-            reportViewer.LocalReport.ReportEmbeddedResource = "PolyclinicMeteringProgram.Report.rdlc";
+            reportViewer.LocalReport.ReportPath = "../../ReportByDate.rdlc";
         }
         private void btnMail_Click(object sender, RoutedEventArgs e)
         {
@@ -33,6 +33,18 @@ namespace PolyclinicMeteringProgram
 
         private void btnShow_Click(object sender, RoutedEventArgs e)
         {
+            if (dpFrom.SelectedDate == null)
+            {
+                MessageBox.Show("Выберите дату начала",
+               "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+            if (dpTo.SelectedDate == null)
+            {
+                MessageBox.Show("Выберите дату окончания",
+               "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
             if (dpFrom.SelectedDate >= dpTo.SelectedDate)
             {
                 MessageBox.Show("Дата начала должна быть меньше даты окончания",
@@ -47,8 +59,9 @@ namespace PolyclinicMeteringProgram
                     DateFrom = dpFrom.SelectedDate,
                     DateTo = dpTo.SelectedDate
                 });
-                ReportDataSource source = new ReportDataSource("DataSetPatientReceipt", dataSource);
+                ReportDataSource source = new ReportDataSource("DataSetReportByDate", dataSource);
                 reportViewer.LocalReport.DataSources.Add(source);
+               
                 reportViewer.RefreshReport();
             }
             catch (Exception ex)
